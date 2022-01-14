fbf6db659fd4fdf310e2063b75c92f24919d6e9e
Ella0707 <pakelvi77@gmail.com>
2022-01-14 22:41:12 +0300

    pages catalogs, brend

--- js/main.js
+++ js/main.js
@@ -138,39 +138,196 @@ new Swiper('.clients__content', {
     },
 });
 
-
+$(".categories__sort-link").click(function(e) {
+    e.preventDefault();
+    $(".categories__sort-link").removeClass('active');
+    $(this).addClass('active');
+  })
 
 
 
 $('.catalog__select-title').click(function (event) {
     $(this).toggleClass('active').next().slideToggle(300);
-  });
+});
 
 
-  new Swiper('.catalog__cotnent-slider', {
+// $(".catalog__option").click(function(e) {
+//     e.preventDefault();
+//     $(".catalog__option").removeClass('active');
+//     $(this).addClass('active');
+//   })
+
+// new Swiper('.catalog__cotnent-slider', {
+//     slidesPerView: 1,
+//     spaceBetween: 20,
+//     speed: 800,
+//     navigation: {
+//         prevEl: '.catalog__arrow-prev',
+//         nextEl: '.catalog__arrow-next',
+//     },
+
+//     pagination: {
+//         el: '.catalog__pagination',
+//         type: "fraction",
+
+//         formatFractionCurrent: function (number) {
+//             if (number < 10) {
+//                 number = "0" + number;
+//             }
+//             return number;
+//         },
+//         formatFractionTotal: function (number) {
+//             if (number < 10) {
+//                 number = "0" + number;
+//             }
+//             return number;
+//         },
+//     },
+// });
+
+
+// new Swiper('.catalog__products-slider', {
+//     slidesPerView: 1,
+//     spaceBetween: 20,
+//     speed: 800,
+//     navigation: {
+//         prevEl: '.catalog__arrow-prev',
+//         nextEl: '.catalog__arrow-next',
+//     },
+
+//     pagination: {
+//         el: '.catalog__pagination',
+//         type: "fraction",
+
+//         formatFractionCurrent: function (number) {
+//             if (number < 10) {
+//                 number = "0" + number;
+//             }
+//             return number;
+//         },
+//         formatFractionTotal: function (number) {
+//             if (number < 10) {
+//                 number = "0" + number;
+//             }
+//             return number;
+//         },
+//     },
+// });
+
+
+
+
+
+$("#rangeSlider").ionRangeSlider();            
+
+var $range = $(".js-range-slider"),
+    $inputFrom = $(".js-input-from"),
+    $inputTo = $(".js-input-to"),
+    instance,
+    min = 1000,
+    max = 3000,
+    from = 0,
+    to = 0;
+
+$range.ionRangeSlider({
+    type: "double",
+    min: min,
+    max: max,
+    from: 1000,
+    to: 2000,
+    hide_from_to: true,
+    hide_min_max: true,
+    onStart: updateInputs,
+    onChange: updateInputs
+});
+instance = $range.data("ionRangeSlider");
+
+function updateInputs (data) {
+	from = data.from;
+    to = data.to;
+    
+    $inputFrom.prop("value", from);
+    $inputTo.prop("value", to);	
+}
+
+$inputFrom.on("input", function () {
+    var val = $(this).prop("value");
+    
+    // validate
+    if (val < min) {
+        val = min;
+    } else if (val > to) {
+        val = to;
+    }
+    
+    instance.update({
+        from: val
+    });
+});
+
+$inputTo.on("input", function () {
+    var val = $(this).prop("value");
+    
+    // validate
+    if (val < from) {
+        val = from;
+    } else if (val > max) {
+        val = max;
+    }
+    
+    instance.update({
+        to: val
+    });
+});
+ 
+
+
+new Swiper('.catalog__product-slider', {
     slidesPerView: 1,
-    spaceBetween: 20,
     speed: 800,
-    navigation: {
-        prevEl: '.catalog__arrow-prev',
-        nextEl: '.catalog__arrow-next',
-    },
+    spaceBetween: 0,
 
     pagination: {
-        el: '.catalog__pagination',
-        type: "fraction",
+    el: '.catalog__product-pagination',
+    clickable: true,
+},
+});
 
-        formatFractionCurrent: function (number) {
-            if (number < 10) {
-                number = "0" + number;
-            }
-            return number;
-        },
-        formatFractionTotal: function (number) {
-            if (number < 10) {
-                number = "0" + number;
-            }
-            return number;
-        },
+
+
+new Swiper('.card-product__description-slider', {
+    slidesPerView: 1,
+    speed: 800,
+    navigation: {
+        prevEl: '.card-product__arrow-prev',
+        nextEl: '.card-product__arrow-next',
     },
 });
+
+
+
+$(".card-product__about-item").click(function(e) {
+    e.preventDefault();
+    $(".card-product__about-item").removeClass('active');
+    $(this).addClass('active');
+  })
+
+
+
+  new Swiper('.product-card', {
+    slidesPerView: 1,
+    speed: 800,
+    spaceBetween: 0,
+
+    pagination: {
+    el: '.product-card__pagination',
+    clickable: true,
+},
+});
+
+
+$(".arrow-custom").click(function(e) {
+    e.preventDefault();
+    $(".arrow-custom").removeClass('active');
+    $(this).addClass('active');
+  })
